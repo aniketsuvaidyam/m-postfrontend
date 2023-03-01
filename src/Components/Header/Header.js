917ecf081d028350989744e8df3b0ec3e7597377
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-03-01 13:11:24 +0530

    add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,133 @@
-import React from 'react'
+import React, {useContext, useEffect, useState } from 'react'
+import Vector from '../../Assets/Vector.png'
+import Avatar from '../../Assets/avatar.png'
+import Navbar from './Navbar/Navbar'
+import { MdOutlineDarkMode } from "react-icons/md";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { BiLogOutCircle } from "react-icons/bi";
+import { BsSearch } from "react-icons/bs";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
+import Profile from './Profile/Profile'
+// import { useSelector } from 'react-redux'
+import { motion } from 'framer-motion';
+import { DataContext } from '../Context/DataProvider'
 
-const Header = () => {
+const Header = ({setTab,tab}) => {
+  const {url , setMsg,setError } =useContext(DataContext)
+const [openProfile, setOpenProfile] = useState(false)
+const [Profileurl, setProfileUrl] = useState(null)
+// const items = useSelector((state) => state.ProfileReducer.url);
+
+
+
+// let token = sessionStorage.getItem('token')
+//     let headers = {
+//       token
+//     }
+// const paylode=sessionStorage.getItem('paylode')
+//     const{_id} =JSON.parse(paylode) 
+    
+const getImg =()=>{
+  axios.get(`http://localhost:4000/employee`,
+  {
+    // headers
+  }).then((res) => {
+    setProfileUrl(res.data.user.url)
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+useEffect(() => {
+return () => {
+  getImg()
+  // setProfileUrl(items)
+}
+},[url])
+
+  const navigate = useNavigate();
+  const signout = async () => {
+    let token = sessionStorage.getItem('token')
+    if (token) {
+      axios.post(`http://localhost:4000/auth/logout`, {},
+        {
+          headers: {
+            "token": ` ${token}`
+          }
+        })
+        .then((res) => {
+          console.log(res);
+          setMsg(res.data.message);
+          setError(true);
+          // localStorage.clear('workSpace')
+          setTimeout(() => {
+            setError(false)
+            navigate("/");
+          }, 1000);
+        })
+        .catch((err) => console.log(err))
+      sessionStorage.removeItem('token')
+    } else {
+      console.log("token require")
+    }
+  
+  
+  }
   return (
     <>
-       <div className="w-full bg-slate-500 h-[10vh] ">
-        </div> 
+     <div className="w-full h-[10vh] shadow-md bg-white z-50">
+        <div className="w-full h-full  flex justify-between items-center px-4 max-w-[1720px] mx-auto">
+          <div className="w-full h-full flex items-center gap-8">
+            {/* Logo */}
+            <div ><img src={Vector} alt=""  className='w-4/5'/></div>
+            {/* navbar */}
+            <Navbar {...{setTab,tab}}/>
+          </div>
+          <div className="flex gap-5 items-center">
+            {/* search */}
+            {/* <div className='cursor-pointer hidden lg:block'><SearchMenu/></div> */}
+           <div className="block lg:hidden">
+           <div className='cursor-pointer flex items-center justify-center w-8 h-8 border rounded-full'>
+              <BsSearch/>
+              </div>
+           </div>
+            {/* dark mode */}
+            <motion.div  whileTap={{ scale: 0.75 }}  className='cursor-pointer w-8 h-8 border-2 rounded-full flex justify-center items-center
+            hover:border-blue-300 group'>
+              <MdOutlineDarkMode className='group-hover:text-blue-600'/>
+              </motion.div>
+            {/* Notification */}
+           <motion.div  whileTap={{ scale: 0.75 }} className='cursor-pointer w-8 h-8 border-2 rounded-full flex justify-center items-center
+            hover:border-blue-300 relative group '>
+             <IoMdNotificationsOutline className='group-hover:text-blue-600'/>
+             <p className='w-1.5 h-1.5 rounded-full bg-red-600 absolute top-2 right-2'></p>
+             </motion.div> 
+           {/* Profile */}
+           <div className="w-12 h-12 border-2 border-blue-500 rounded-full cursor-pointer 
+           relative flex flex-col items-center group">
+            <img className='w-12 h-12 border-2 rounded-full object-cover' src={Profileurl ? 'http://localhost:4000/' + Profileurl : Avatar} alt="" />
+           <div className="w-44 shadow-xl absolute  top-10 right-0
+           hidden group-hover:block rounded-md z-50" >
+            <p className='p-1.5'></p>
+            <ul className='bg-white border rounded-md py-1'>
+            <li className='hover:bg-blue-400 border-b px-2 py-1.5 rounded-t-sm flex justify-between
+            items-center hover:text-white font-medium text-sm' onClick={()=>setOpenProfile(!openProfile)}>Profile <CgProfile/></li>
+            <li className=' hover:bg-red-500 px-2 py-1.5 rounded-b-sm flex 
+            justify-between  items-center hover:text-white font-medium text-sm' onClick={signout}>Logout 
+            <BiLogOutCircle/></li>
+            </ul>
+           </div>
+           </div>
+           {/* Share */}
+           <motion.button whileTap={{ scale: 0.90 }}
+           className='bg-blue-600 text-white py-1 w-20 rounded-sm'>SHARE</motion.button>
+          </div>
+        </div>
+        {/* Profile */}
+        {openProfile===true? <Profile setOpenProfile={setOpenProfile}/>:null}
+       
+     </div>
     </>
   )
 }
---
+++ src/Components/Header/Profile/Profile.js
@@ -0,0 +1,130 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Avatar from '../../../Assets/avatar.png'
+import {TbUpload} from 'react-icons/tb'
+import {HiOutlineTrash} from 'react-icons/hi'
+// import { useDispatch, useSelector } from 'react-redux';
+// import { ProfileUrl } from '../../../Redux/Action/ProfileAction'
+import { Puff } from  'react-loader-spinner'
+import Http from '../../../Service/Http'
+import { DataContext } from '../../Context/DataProvider';
+
+const Profile = ({setOpenProfile}) => {
+    
+    const [file, setfile] = useState(null)
+    const {url, setUrl,setMsg,setError,setStatus} =useContext(DataContext)
+    const [isLoading, setLoading] = useState(false);
+    // const dispatch = useDispatch()
+    // const items = useSelector((state) => state.ProfileReducer.url)
+    const paylode=sessionStorage.getItem('paylode')
+    const{_id} =JSON.parse(paylode) 
+
+    const Upload=()=>{
+      // dispatch(ProfileUrl(url))
+      // setUrl(items)
+        const body = new FormData()
+    body.append('file', file)
+
+    Http({
+      method: "put",
+      url: `${process.env.REACT_APP_BASEURL}/employee/updateImage/${_id}`,
+      data:body
+    }).then((res) => {
+      setMsg(res.data.message)
+      setStatus(res.status)
+      setError(true)
+        setUrl(null)
+      }).catch((error) => {
+        setMsg(error.response.data.message)
+        setStatus(error.response.status)
+        setError(true)
+      })
+    }
+    const getImg =()=>{
+      Http({
+          method: "get",
+          url: `${process.env.REACT_APP_BASEURL}/employee/${_id}`,
+        }).then((res) => {
+          setUrl(res.data.user.url)
+          setLoading(true);
+          setTimeout(() => {
+          setLoading(false);
+          }, 1000);
+        }).catch((error) => {
+          console.log(error)
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        })
+    }
+    const deleteImg =()=>{
+      Http({
+        method: "delete",
+        url: `${process.env.REACT_APP_BASEURL}/employee/deletePhoto`,
+      }).then((res) => {
+        setUrl(res.data.user.url)
+        setMsg(res.data.message)
+        setStatus(res.status)
+      setError(true)
+        setLoading(true);
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      }).catch((error) => {
+        setMsg(error.response.data.message)
+        setStatus(error.response.status)
+        setError(true)
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+      })
+    }
+    useEffect(() => {
+      return () => {
+        getImg()
+      }
+    }, [url])
+    
+  return (
+    <>
+        <div className="w-full h-full flex justify-center items-center 
+         fixed z-[1000] top-0 inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+            <div className="w-[450px] h-[350px] bg-white rounded-md shadow-xl p-7 flex flex-col justify-between">
+                <h1 className='text-xl flex font-medium'>Change your profile picture</h1>
+                <div className='flex justify-between w-full'>
+                  <div className="w-32 h-32 border-2 border-blue-400 rounded-full cursor-pointer flex justify-center items-center">
+                    {isLoading===false?<img className='w-[126px] h-[125px] rounded-full object-cover' src={url?`http://localhost:4000/`+url:Avatar} alt="" />
+                    :<Puff
+                    height="80"
+                    width="80"
+                    radisu={1}
+                    color="#4fa94d"
+                    ariaLabel="puff-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                  />}
+                  </div>
+                  <div className='py-5 flex flex-col gap-4'>
+                  <label htmlFor="file" className='cursor-pointer flex items-center gap-2 hover:text-blue-600'>
+                    <TbUpload/> Upload picture
+                  <input type="file" id='file' className='h-0 w-0' onChange={(e)=>setfile(e.target.files[0])}/>
+                  </label>
+                  <p className='flex items-center cursor-pointer gap-2 hover:text-blue-600' onClick={deleteImg}>
+                    <HiOutlineTrash/> Remove picture</p>
+                  </div>
+                </div>
+                <div className="w-full flex justify-between px-3">
+                   <button className='border px-8 py-1 rounded-md font-medium 
+                    hover:bg-blue-600 hover:text-white' onClick={Upload}>Save</button>
+                   <button className='border px-8 py-1 rounded-md font-medium hover:bg-blue-600 hover:text-white'
+                    onClick={()=>setOpenProfile(false)}>Close</button>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Profile
