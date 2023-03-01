d4deff5d8951737a88ea985d8174ea9b1eeb6f96
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-03-01 18:04:42 +0530

    right bar fixed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Vector from '../../Assets/Vector.png'
 import Avatar from '../../Assets/avatar.png'
 import Navbar from './Navbar/Navbar'
@@ -15,37 +15,37 @@ import { motion } from 'framer-motion';
 import { DataContext } from '../Context/DataProvider'
 import Search from '../Search/Search';
 
-const Header = ({setTab,tab}) => {
-  const {url , setMsg,setError } =useContext(DataContext)
-const [openProfile, setOpenProfile] = useState(false)
-const [Profileurl, setProfileUrl] = useState(null)
-// const items = useSelector((state) => state.ProfileReducer.url);
+const Header = ({ setTab, tab }) => {
+  const { url, setMsg, setError } = useContext(DataContext)
+  const [openProfile, setOpenProfile] = useState(false)
+  const [Profileurl, setProfileUrl] = useState(null)
+  // const items = useSelector((state) => state.ProfileReducer.url);
 
 
 
-let token = sessionStorage.getItem('token')
-    let headers = {
-      token
+  let token = sessionStorage.getItem('token')
+  let headers = {
+    token
+  }
+  const paylode = sessionStorage.getItem('paylode')
+  const { _id } = JSON.parse(paylode)
+
+  const getImg = () => {
+    axios.get(`http://localhost:4000/employee/${_id}`,
+      {
+        headers
+      }).then((res) => {
+        setProfileUrl(res.data.user.url)
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
+  useEffect(() => {
+    return () => {
+      getImg()
+      // setProfileUrl(items)
     }
-const paylode=sessionStorage.getItem('paylode')
-    const{_id} =JSON.parse(paylode) 
-    
-const getImg =()=>{
-  axios.get(`http://localhost:4000/employee/${_id}`,
-  {
-    headers
-  }).then((res) => {
-    setProfileUrl(res.data.user.url)
-  }).catch((error) => {
-    console.log(error)
-  })
-}
-useEffect(() => {
-return () => {
-  getImg()
-  // setProfileUrl(items)
-}
-},[url])
+  }, [url])
 
   const navigate = useNavigate();
   const signout = async () => {
@@ -72,63 +72,63 @@ return () => {
     } else {
       console.log("token require")
     }
-  
-  
+
+
   }
   return (
     <>
-     <div className="w-full h-[10vh] shadow-md bg-white z-50">
+      <div className="w-full h-[10vh] min-h-[62px] shadow-md bg-white z-50">
         <div className="w-full h-full  flex justify-between items-center px-4 max-w-[1720px] mx-auto">
           <div className="w-full h-full flex items-center gap-8">
             {/* Logo */}
-            <div ><img src={Vector} alt=""  className='w-4/5'/></div>
+            <div ><img src={Vector} alt="" className='w-4/5' /></div>
             {/* navbar */}
-            <Navbar {...{setTab,tab}}/>
+            <Navbar {...{ setTab, tab }} />
           </div>
           <div className="flex gap-5 items-center">
             {/* search */}
-            <div className='cursor-pointer hidden lg:block'><Search/></div>
-           <div className="block lg:hidden">
-           <div className='cursor-pointer flex items-center justify-center w-8 h-8 border rounded-full'>
-              <BsSearch/>
+            <div className='cursor-pointer hidden lg:block'><Search /></div>
+            <div className="block lg:hidden">
+              <div className='cursor-pointer flex items-center justify-center w-8 h-8 border rounded-full'>
+                <BsSearch />
               </div>
-           </div>
+            </div>
             {/* dark mode */}
-            <motion.div  whileTap={{ scale: 0.75 }}  className='cursor-pointer w-8 h-8 border-2 rounded-full flex justify-center items-center
+            <motion.div whileTap={{ scale: 0.75 }} className='cursor-pointer w-8 h-8 border-2 rounded-full flex justify-center items-center
             hover:border-blue-300 group'>
-              <MdOutlineDarkMode className='group-hover:text-blue-600'/>
-              </motion.div>
+              <MdOutlineDarkMode className='group-hover:text-blue-600' />
+            </motion.div>
             {/* Notification */}
-           <motion.div  whileTap={{ scale: 0.75 }} className='cursor-pointer w-8 h-8 border-2 rounded-full flex justify-center items-center
+            <motion.div whileTap={{ scale: 0.75 }} className='cursor-pointer w-8 h-8 border-2 rounded-full flex justify-center items-center
             hover:border-blue-300 relative group '>
-             <IoMdNotificationsOutline className='group-hover:text-blue-600'/>
-             <p className='w-1.5 h-1.5 rounded-full bg-red-600 absolute top-2 right-2'></p>
-             </motion.div> 
-           {/* Profile */}
-           <div className="w-12 h-12 border-2 border-blue-500 rounded-full cursor-pointer 
+              <IoMdNotificationsOutline className='group-hover:text-blue-600' />
+              <p className='w-1.5 h-1.5 rounded-full bg-red-600 absolute top-2 right-2'></p>
+            </motion.div>
+            {/* Profile */}
+            <div className="w-12 h-12 border-2 border-blue-500 rounded-full cursor-pointer 
            relative flex flex-col items-center group">
-            <img className='w-12 h-12 border-2 rounded-full object-cover' src={Profileurl ? 'http://localhost:4000/' + Profileurl : Avatar} alt="" />
-           <div className="w-44 shadow-xl absolute  top-10 right-0
+              <img className='w-12 h-12 border-2 rounded-full object-cover' src={Profileurl ? 'http://localhost:4000/' + Profileurl : Avatar} alt="" />
+              <div className="w-44 shadow-xl absolute  top-10 right-0
            hidden group-hover:block rounded-md z-50" >
-            <p className='p-1.5'></p>
-            <ul className='bg-white border rounded-md py-1'>
-            <li className='hover:bg-blue border-b px-2 py-1.5 rounded-t-sm flex justify-between
-            items-center hover:text-white font-medium text-sm' onClick={()=>setOpenProfile(!openProfile)}>Profile <CgProfile/></li>
-            <li className=' hover:bg-red-500 px-2 py-1.5 rounded-b-sm flex 
-            justify-between  items-center hover:text-white font-medium text-sm' onClick={signout}>Logout 
-            <BiLogOutCircle/></li>
-            </ul>
-           </div>
-           </div>
-           {/* Share */}
-           <motion.button whileTap={{ scale: 0.90 }}
-           className='bg-blue text-white py-1 w-20 rounded-sm'>SHARE</motion.button>
+                <p className='p-1.5'></p>
+                <ul className='bg-white border rounded-md py-1'>
+                  <li className='hover:bg-blue border-b px-2 py-1.5 rounded-t-sm flex justify-between
+            items-center hover:text-white font-medium text-sm' onClick={() => setOpenProfile(!openProfile)}>Profile <CgProfile /></li>
+                  <li className=' hover:bg-red-500 px-2 py-1.5 rounded-b-sm flex 
+            justify-between  items-center hover:text-white font-medium text-sm' onClick={signout}>Logout
+                    <BiLogOutCircle /></li>
+                </ul>
+              </div>
+            </div>
+            {/* Share */}
+            <motion.button whileTap={{ scale: 0.90 }}
+              className='bg-blue text-white py-1 w-20 rounded-sm'>SHARE</motion.button>
           </div>
         </div>
         {/* Profile */}
-        {openProfile===true? <Profile setOpenProfile={setOpenProfile}/>:null}
-       
-     </div>
+        {openProfile === true ? <Profile setOpenProfile={setOpenProfile} /> : null}
+
+      </div>
     </>
   )
 }
--- src/Components/WorkSpace/RightSideUI/NavBody/NavBody.js
+++ src/Components/WorkSpace/RightSideUI/NavBody/NavBody.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsCaretRight, BsCode } from "react-icons/bs";
+import { FiTrash2 } from "react-icons/fi";
+import { BiHelpCircle } from "react-icons/bi";
+import { motion } from "framer-motion";
+// import ModelBox from "../../ModelBox/ModelBox";
 
-const NavBody = () => {
+const RightBar = () => {
+  const [open, setOpenn] = useState(false);
+
+  const envirmentOpen = () => {
+    setOpenn(!open);
+  };
+
   return (
     <>
-      <div className="min-w-[80px] h-[90vh] bg-orange-500"></div>
+      <div className=" h-[90vh] bg-orange-500  relative">
+        <div className="flex flex-col justify-between items-center w-12 min-w-[48px] bg-white h-full py-1.5">
+          <div className="flex flex-col gap-1">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className={`hover:bg-blue-200 w-8 h-8 flex justify-center items-center
+                             cursor-pointer rounded-md  group ${open === true ? "bg-blue-200" : null
+                }`}
+              onClick={envirmentOpen}
+            >
+              {/* <ModelBox {...{ set: setOpenn }} /> */}
+            </motion.div>
+
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="hover:bg-blue-200 w-8 h-8 flex justify-center items-center 
+                             rounded-md cursor-pointer group"
+            >
+              <BsCode className=" group-hover:text-blue-600" />
+            </motion.div>
+          </div>
+          <div className="flex flex-col gap-0.5">
+            <div
+              className="hover:bg-blue-200 w-8 h-8 flex justify-center items-center 
+                             rounded-md cursor-pointer group"
+            >
+              <BsCaretRight className=" group-hover:text-blue-600" />
+            </div>
+            <p className="text-[10px] w-full flex justify-center">Runner</p>
+            <div
+              className="hover:bg-blue-200 w-8 h-8 flex justify-center items-center 
+                             rounded-md cursor-pointer group"
+            >
+              <FiTrash2 className=" group-hover:text-blue-600" />
+            </div>
+            <p className="text-[10px] w-full flex justify-center">Trash</p>
+            <div
+              className="hover:bg-blue-200 w-8 h-8 flex justify-center items-center 
+                             rounded-md cursor-pointer group"
+            >
+              <BiHelpCircle className=" group-hover:text-blue-600" />
+            </div>
+            <p className="text-[10px] w-full flex justify-center">Help</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default NavBody;
+export default RightBar;
+
+
+
+{/* <div className="min-w-[80px] h-[90vh] bg-orange-500">
+        <div className="w-12 min-w-[48px] bg-white h-full">
+          
+        </div>
+      </div> */}
