15e87020c147f23ee724a19965b327a05fe321a9
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-03-02 07:45:55 +0530

    tabs add

--- src/Components/WorkSpace/LeftSideUI/LeftNav/LeftNav.js
+++ src/Components/WorkSpace/LeftSideUI/LeftNav/LeftNav.js
@@ -1,12 +1,62 @@
-import React from 'react'
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
+import { MdOutlineCollectionsBookmark, MdHistory } from "react-icons/md";
+import { BiCodeBlock, BiCollapse } from "react-icons/bi";
 
 const LeftNav = () => {
   return (
     <>
-      <div className="min-w-[80px] h-full bg-orange-400 flex justify-end flex-col">sdd
+      <div className="w-20 flex h-full">
+        <div className=" w-full min-w-[80px] px-1 flex flex-col h-full border-r-[1.5px] gap-1 ">
+          <NavLink
+            to="collection"
+            className="w-full  flex flex-col items-center justify-center rounded  mt-1 py-3 text-xs gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? "#e8eeff" : "",
+              color: isActive ? "#2B26FF" : "",
+            })}
+          >
+            <MdOutlineCollectionsBookmark className="text-xl" />
+            Collection
+          </NavLink>
+
+          <NavLink
+            to="api"
+            className="text-xs flex flex-col items-center  rounded  justify-center py-3 gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
+          >
+            <BiCollapse className="text-xl" />
+            APIs
+          </NavLink>
+          <NavLink
+            to="environment"
+            className="text-xs flex flex-col items-center  rounded  justify-center py-3 gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
+          >
+            <BiCodeBlock className="text-xl" />
+            Environment
+          </NavLink>
+          <NavLink
+            to="history"
+            className="text-xs flex flex-col items-center  rounded  justify-center py-3 gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
+          >
+            <MdHistory className="text-xl" />
+            History
+          </NavLink>
+        </div>
+        <div className="w-full">
+          <Outlet />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default LeftNav
+export default LeftNav;
